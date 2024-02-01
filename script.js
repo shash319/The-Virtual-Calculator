c9312d5a65f8f1839b4bfba77e06475473d63f70
Shasawat Pal <shashwatpal2001@gmail.com>
2024-02-01 13:02:25 +0530

    final back

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ function calculate(){
     document.getElementById("result").value = q; 
 }
 
+let Back=()=> {
+    var ev = document.getElementById('result');
+    ev.value = ev.value.slice(0,-1);
+ } 
+
 // const keyinput = document.getElementById("display-box");
 // document.addEventListener('keypress', (e) => {
 //     keyinput.value += e.key;
